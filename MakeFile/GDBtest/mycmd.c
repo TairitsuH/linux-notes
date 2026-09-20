3a196e069d812da49d7d70dab5f2b21f46517684
TairitsuH <tairitsu_h@qq.com>
2026-09-20 15:37:25 +0800

    测试make用法

--- MakeFile/GDBtest/mycmd.c
+++ MakeFile/GDBtest/mycmd.c
@@ -1,27 +1,7 @@
-// mycmd.c
-#include <stdio.h>
-
-int Sum(int s, int e)
-{
-    int result = 0;
-    for (int i = s; i <= e; i++)
-    {
-        result += i;
-    }
-    return result;
-}
+#include<stdio.h>
 
 int main()
 {
-    int start = 1;
-    int end = 100;
-
-    printf("I will begin\n");
-
-    int n = Sum(start, end);
-
-    printf("running done, result is: [%d-%d]=%d\n", start, end, n);
-
+    printf("hello linux\n");
     return 0;
 }
-
